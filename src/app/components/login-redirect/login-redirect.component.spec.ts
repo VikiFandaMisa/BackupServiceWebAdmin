3c1b12ba3648cba1943b89e716bbc084b440a8d8
Michal Černý <cernyymichal@gmail.com>
2020-06-07 22:41:37 +0200

    beep

--- src/app/components/login-redirect/login-redirect.component.spec.ts
+++ src/app/components/login-redirect/login-redirect.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LoginRedirectComponent } from './login-redirect.component';
+import { LoginRedirectComponent } from "./login-redirect.component";
 
-describe('LoginRedirectComponent', () => {
-  let component: LoginRedirectComponent;
-  let fixture: ComponentFixture<LoginRedirectComponent>;
+describe("LoginRedirectComponent", () => {
+    let component: LoginRedirectComponent;
+    let fixture: ComponentFixture<LoginRedirectComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginRedirectComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginRedirectComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginRedirectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginRedirectComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
